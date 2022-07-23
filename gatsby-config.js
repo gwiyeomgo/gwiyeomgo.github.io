44ad04136406a7ecf0369a3eb7935cb0389c74ee
gwiyeomgo <gwiyeomgo@gmail.com>
2022-07-23 13:16:23 +0900

    20220723- 구글에널릭티그스랑 서치콘솔 적용

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,7 +60,9 @@ module.exports = {
                 ],
             },
         },
-        `gatsby-plugin-sitemap`,
+        `gatsby-plugin-advanced-sitemap`,
+        `gatsby-plugin-robots-txt`,
+        `gatsby-plugin-react-helmet`,
         {
             resolve: `gatsby-plugin-manifest`,
             options: {
@@ -137,11 +139,27 @@ module.exports = {
             },
         },
         {
-            resolve: `gatsby-plugin-gtag`,
+            resolve: `gatsby-plugin-google-gtag`,
             options: {
-                trackingId: 'UA-219953705-1',
-                head: false,
-                anonymize: true,
+                trackingIds: ['G-P3DYW3SB5H','UA-219953705-1'],
+                // This object gets passed directly to the gtag config command
+                // This config will be shared across all trackingIds
+                gtagConfig: {
+                    optimize_id: "OPT_CONTAINER_ID",
+                    anonymize_ip: true,
+                    cookie_expires: 0,
+                },
+                // This object is used for configuration specific to this plugin
+                pluginConfig: {
+                    // Puts tracking script in the head instead of the body
+                    head: false,
+                    // Setting this parameter is also optional
+                    respectDNT: true,
+                    // Avoids sending pageview hits from custom paths
+                    exclude: ["/preview/**", "/do-not-track/me/too/"],
+                    // Defaults to https://www.googletagmanager.com
+                    origin: "YOUR_SELF_HOSTED_ORIGIN",
+                },
             },
         },
         `gatsby-plugin-gatsby-cloud`,
