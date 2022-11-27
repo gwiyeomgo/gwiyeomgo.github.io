ed5ab625c6f07719a43ddce9287c5e0ba7219525
gwiyeomgo <gwiyeomgo@gmail.com>
2022-11-28 00:28:39 +0900

    20221128 blog Template upgrade

--- src/@lekoarts/gatsby-theme-minimal-blog/components/navigation.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/navigation.tsx
@@ -6,33 +6,33 @@ import useMinimalBlogConfig from "@lekoarts/gatsby-theme-minimal-blog/src/hooks/
 import replaceSlashes from "@lekoarts/gatsby-theme-minimal-blog/src/utils/replaceSlashes"
 
 type NavigationProps = {
-    nav: {
-        title: string
-        slug: string
-    }[]
+  nav: {
+    title: string
+    slug: string
+  }[]
 }
 
 const Navigation = ({ nav }: NavigationProps) => {
-    const { basePath } = useMinimalBlogConfig()
+  const { basePath } = useMinimalBlogConfig()
 
-    return (
-        <React.Fragment>
-            {nav && nav.length > 0 && (
-                <nav sx={{ "a:not(:last-of-type)": { mr: 3 }, fontSize: [1, `18px`], ".active": { color: `heading` } }}>
-                    {nav.map((item) => (
-                        <Link
-                            activeClassName="active"
-                            key={item.slug}
-                            sx={(t) => ({ ...t.styles?.a })}
-                            to={replaceSlashes(`/${basePath}/${item.slug}`)}
-                        >
-                            <img src={item.title}/>
-                        </Link>
-                    ))}
-                </nav>
-            )}
-        </React.Fragment>
-    )
+  return (
+    <React.Fragment>
+      {nav && nav.length > 0 && (
+        <nav sx={{ "a:not(:last-of-type)": { mr: 3 }, fontSize: [1, `18px`], ".active": { color: `heading` } }}>
+          {nav.map((item) => (
+            <Link
+              activeClassName="active"
+              key={item.slug}
+              sx={(t) => ({ ...t.styles?.a })}
+              to={replaceSlashes(`/${basePath}/${item.slug}`)}
+            >
+              <img src={item.title}/>
+            </Link>
+          ))}
+        </nav>
+      )}
+    </React.Fragment>
+  )
 }
 
 export default Navigation
--- src/@lekoarts/gatsby-theme-minimal-blog/components/post.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/post.tsx
@@ -1,76 +1,82 @@
 /** @jsx jsx */
+import type { HeadFC, PageProps } from "gatsby"
 import * as React from "react"
 import { jsx, Heading } from "theme-ui"
-import { MDXRenderer } from "gatsby-plugin-mdx"
 import Layout from "@lekoarts/gatsby-theme-minimal-blog/src/components/layout"
 import ItemTags from "@lekoarts/gatsby-theme-minimal-blog/src/components/item-tags"
 import Seo from "./seo"
-import { Disqus } from "gatsby-plugin-disqus";
+import PostFooter from "@lekoarts/gatsby-theme-minimal-blog/src/components/post-footer"
 
-type PostProps = {
-    data: {
-        post: {
-            slug: string
-            title: string
-            date: string
-            tags?: {
-                name: string
-                slug: string
-            }[]
-            description?: string
-            canonicalUrl?: string
-            body: string
-            excerpt: string
-            timeToRead?: number
-            banner?: {
-                childImageSharp: {
-                    resize: {
-                        src: string
-                    }
-                }
-            }
+export type MBPostProps = {
+  post: {
+    slug: string
+    title: string
+    date: string
+    tags?: {
+      name: string
+      slug: string
+    }[]
+    description?: string
+    canonicalUrl?: string
+    excerpt: string
+    timeToRead?: number
+    banner?: {
+      childImageSharp: {
+        resize: {
+          src: string
         }
+      }
     }
+  }
 }
 
-const px = [`32px`, `16px`, `8px`, `4px`]
-const shadow = px.map((v) => `rgba(0, 0, 0, 0.15) 0px ${v} ${v} 0px`)
+const px = [`16px`, `8px`, `4px`]
+const shadow = px.map((v) => `rgba(0, 0, 0, 0.1) 0px ${v} ${v} 0px`)
 
-
-const Post = ({ data: { post } }: PostProps) => (
-    <Layout>
-        <Seo
-            title={post.title}
-            description={post.description ? post.description : post.excerpt}
-            image={post.banner ? post.banner?.childImageSharp?.resize?.src : undefined}
-            pathname={post.slug}
-            canonicalUrl={post.canonicalUrl}
-        />
-        <Heading as="h2" variant="styles.h2">
-            {post.title}
-        </Heading>
-        <p sx={{ color: `secondary`, mt: 3, a: { color: `secondary` }, fontSize: [1, 1, 2] }}>
-            <time>{post.date}</time>
-            {post.tags && (
-                <React.Fragment>
-                    {` — `}
-                    <ItemTags tags={post.tags} />
-                </React.Fragment>
-            )}
-            {post.timeToRead && ` — `}
-            {post.timeToRead && <span>{post.timeToRead} min read</span>}
-        </p>
-        <section
-            sx={{
-                my: 5,
-                ".gatsby-resp-image-wrapper": { my: [4, 4, 5], boxShadow: shadow.join(`, `) },
-                variant: `layout.content`,
-            }}
-        >
-           <MDXRenderer>{post.body}</MDXRenderer>
-        </section>
-        <Disqus />
-    </Layout>
+const Post: React.FC<React.PropsWithChildren<PageProps<MBPostProps>>> = ({ data: { post }, children }) => (
+  <Layout>
+    <Heading as="h1" variant="styles.h1">
+      {post.title}
+    </Heading>
+    <p sx={{ color: `secondary`, mt: 3, a: { color: `secondary` }, fontSize: [1, 1, 2] }}>
+      <time>{post.date}</time>
+      {post.tags && (
+        <React.Fragment>
+          {` — `}
+          <ItemTags tags={post.tags} />
+        </React.Fragment>
+      )}
+      {post.timeToRead && ` — `}
+      {post.timeToRead && <span>{post.timeToRead} min read</span>}
+    </p>
+    <section
+      sx={{
+        my: 5,
+        ".gatsby-resp-image-wrapper": {
+          my: [4, 4, 5],
+          borderRadius: `4px`,
+          boxShadow: shadow.join(`, `),
+          ".gatsby-resp-image-image": {
+            borderRadius: `4px`,
+          },
+        },
+        variant: `layout.content`,
+      }}
+    >
+      {children}
+    </section>
+    <PostFooter post={post} />
+  </Layout>
 )
 
 export default Post
+
+export const Head: HeadFC<MBPostProps> = ({ data: { post } }) => (
+  <Seo
+    title={post.title}
+    description={post.description ? post.description : post.excerpt}
+    image={post.banner ? post.banner?.childImageSharp?.resize?.src : undefined}
+    pathname={post.slug}
+    canonicalUrl={post.canonicalUrl}
+  />
+)
