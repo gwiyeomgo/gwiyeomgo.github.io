0af704044de1fdfc4b8b3478bbbc843dd2a34e50
gwiyeomgo <gwiyeomgo@gmail.com>
2022-12-12 00:47:27 +0900

    20221212

---
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/code.tsx
@@ -0,0 +1,75 @@
+import * as React from "react"
+import { useColorMode } from "theme-ui"
+import Highlight, { defaultProps } from "prism-react-renderer"
+import { calculateLinesToHighlight, getLanguage, GetLanguageInput } from "@lekoarts/themes-utils"
+import lightTheme from "prism-react-renderer/themes/github"
+import darkTheme from "prism-react-renderer/themes/vsDark"
+import useMinimalBlogConfig from "@lekoarts/gatsby-theme-minimal-blog/src/hooks/use-minimal-blog-config"
+
+type CodeProps = {
+  codeString: string
+  withLineNumbers?: boolean
+  highlight?: string
+  title?: string
+  className: GetLanguageInput
+}
+
+const Code = ({
+  codeString,
+  withLineNumbers = false,
+  title = ``,
+  className: blockClassName,
+  highlight = ``,
+}: CodeProps) => {
+  const { showLineNumbers, showCopyButton } = useMinimalBlogConfig()
+  const [colorMode] = useColorMode()
+  const isDark = colorMode === `dark`
+
+  const language = getLanguage(blockClassName)
+  const shouldHighlightLine = calculateLinesToHighlight(highlight)
+  const shouldShowLineNumbers = withLineNumbers || showLineNumbers
+
+  return (
+    // @ts-ignore
+    <Highlight {...defaultProps} code={codeString} language={language} theme={isDark ? darkTheme : lightTheme}>
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <React.Fragment>
+          <div className="gatsby-highlight" data-language={language}>
+            {title && (
+              <div className="code-title">
+                <div>{title}</div>
+              </div>
+            )}
+            <pre className={className} style={style} data-linenumber={shouldShowLineNumbers}>
+             {/*showCopyButton && <Copy content={codeString} fileName={title} />*/}
+              <code className={`code-content language-${language}`}>
+                {tokens.map((line, i) => {
+                  const lineProps = getLineProps({ line, key: i })
+
+                  if (shouldHighlightLine(i)) {
+                    lineProps.className = `${lineProps.className} highlight-line`
+                    lineProps.style = {
+                      ...lineProps.style,
+                      backgroundColor: isDark ? `rgba(255, 255, 255, 0.1)` : `rgba(0, 0, 0, 0.035)`,
+                    }
+                  }
+
+                  return (
+                    <div {...lineProps}>
+                      {shouldShowLineNumbers && <span className="line-number-style">{i + 1}</span>}
+                      {line.map((token, key) => (
+                        <span {...getTokenProps({ token, key })} />
+                      ))}
+                    </div>
+                  )
+                })}
+              </code>
+            </pre>
+          </div>
+        </React.Fragment>
+      )}
+    </Highlight>
+  )
+}
+
+export default Code
--- src/@lekoarts/gatsby-theme-minimal-blog/components/post.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/post.tsx
@@ -1,97 +1,98 @@
 /** @jsx jsx */
-import type { HeadFC, PageProps } from "gatsby"
+import type {HeadFC, PageProps} from "gatsby"
 import * as React from "react"
-import { jsx, Heading } from "theme-ui"
+import {jsx, Heading} from "theme-ui"
 import Layout from "@lekoarts/gatsby-theme-minimal-blog/src/components/layout"
 import ItemTags from "@lekoarts/gatsby-theme-minimal-blog/src/components/item-tags"
 import Seo from "./seo"
 import PostFooter from "@lekoarts/gatsby-theme-minimal-blog/src/components/post-footer"
-import { Disqus } from "gatsby-plugin-disqus";
+import {Disqus} from "gatsby-plugin-disqus";
 
 
 export type MBPostProps = {
-  post: {
-    slug: string
-    title: string
-    date: string
-    tags?: {
-      name: string
-      slug: string
-    }[]
-    description?: string
-    canonicalUrl?: string
-    excerpt: string
-    timeToRead?: number
-    banner?: {
-      childImageSharp: {
-        resize: {
-          src: string
+    post: {
+        slug: string
+        title: string
+        date: string
+        tags?: {
+            name: string
+            slug: string
+        }[]
+        description?: string
+        canonicalUrl?: string
+        excerpt: string
+        timeToRead?: number
+        banner?: {
+            childImageSharp: {
+                resize: {
+                    src: string
+                }
+            }
         }
-      }
     }
-  }
 }
 
 const px = [`16px`, `8px`, `4px`]
 const shadow = px.map((v) => `rgba(0, 0, 0, 0.1) 0px ${v} ${v} 0px`)
 
-const ScriptHandler = (props) => {
-    // if the script is a gist override it with our react component.
-    if (props.src && props.src.includes('gist.github.com')) {
-        console.log("--------",props.src)
-        return <Gist id={props.src.split('/')[4].split('.')[0]}/>
-    }
-    return null
-}
+const Post: React.FC<React.PropsWithChildren<PageProps<MBPostProps>>> = ({data: {post}, children}) => (
+    <Layout>
+        <Heading as="h1" variant="styles.h1">
+            {post.title}
+        </Heading>
+        <p sx={{color: `secondary`, mt: 3, a: {color: `secondary`}, fontSize: [1, 1, 2]}}>
+            <time>{post.date}</time>
+            {post.tags && (
+                <React.Fragment>
+                    {` — `}
+                    <ItemTags tags={post.tags}/>
+                </React.Fragment>
+            )}
+            {post.timeToRead && ` — `}
+            {post.timeToRead && <span>{post.timeToRead} min read</span>}
+        </p>
+        <section
+            sx={{
+                my: 5,
+                ".gatsby-resp-image-wrapper": {
+                    my: [4, 4, 5],
+                    borderRadius: `4px`,
+                    boxShadow: shadow.join(`, `),
+                    ".gatsby-resp-image-image": {
+                        borderRadius: `4px`,
+                    },
+                },
+                variant: `layout.content`,
+            }}
 
-const Post: React.FC<React.PropsWithChildren<PageProps<MBPostProps>>> = ({ data: { post }, children }) => (
-  <Layout>
-    <Heading as="h1" variant="styles.h1">
-      {post.title}
-    </Heading>
-    <p sx={{ color: `secondary`, mt: 3, a: { color: `secondary` }, fontSize: [1, 1, 2] }}>
-      <time>{post.date}</time>
-      {post.tags && (
-        <React.Fragment>
-          {` — `}
-          <ItemTags tags={post.tags} />
-        </React.Fragment>
-      )}
-      {post.timeToRead && ` — `}
-      {post.timeToRead && <span>{post.timeToRead} min read</span>}
-    </p>
-    <section
-      sx={{
-        my: 5,
-        ".gatsby-resp-image-wrapper": {
-          my: [4, 4, 5],
-          borderRadius: `4px`,
-          boxShadow: shadow.join(`, `),
-          ".gatsby-resp-image-image": {
-            borderRadius: `4px`,
-          },
-        },
-        variant: `layout.content`,
-      }}
-    >
-
-        {children}
-
-
-    </section>
-    <Disqus />
-    <PostFooter post={post} />
-  </Layout>
+        >
+            <div
+                onMouseMove={(e) => {
+                    e.preventDefault()
+                }}
+                onCopy={(e) => {
+                    e.preventDefault()
+                }}
+                onContextMenu={(e) => {
+                    e.preventDefault();
+                }}
+            >
+                {children}
+            </div>
+        </section>
+        <Disqus/>
+        <PostFooter post={post}/>
+    </Layout>
 )
 
 export default Post
 
-export const Head: HeadFC<MBPostProps> = ({ data: { post } }) => (
-  <Seo
-    title={post.title}
-    description={post.description ? post.description : post.excerpt}
-    image={post.banner ? post.banner?.childImageSharp?.resize?.src : undefined}
-    pathname={post.slug}
-    canonicalUrl={post.canonicalUrl}
-  />
+export const Head: HeadFC<MBPostProps> = ({data: {post}}) => (
+    <Seo
+        title={post.title}
+        description={post.description ? post.description : post.excerpt}
+        image={post.banner ? post.banner?.childImageSharp?.resize?.src : undefined}
+        pathname={post.slug}
+        canonicalUrl={post.canonicalUrl}
+    />
 )
