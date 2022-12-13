bf51cd56ef19545a2cb2f9535c28c514152d0c1c
gwiyeomgo <gwiyeomgo@gmail.com>
2022-12-14 01:38:25 +0900

    20221214

---
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/blog-list-item.tsx
@@ -0,0 +1,40 @@
+/** @jsx jsx */
+import * as React from "react"
+import { jsx, Box } from "theme-ui"
+import { Link } from "gatsby"
+import ItemTags from "@lekoarts/gatsby-theme-minimal-blog/src/components/item-tags"
+
+type BlogListItemProps = {
+  post: {
+    slug: string
+    title: string
+    date: string
+    excerpt: string
+    description: string
+    timeToRead?: number
+    tags?: {
+      name: string
+      slug: string
+    }[]
+  }
+  showTags?: boolean
+}
+
+const BlogListItem = ({ post, showTags = true }: BlogListItemProps) => (
+  <Box mb={2}>
+    <Link to={post.slug} sx={(t) => ({ ...t.styles?.a, fontSize: [1, 2, 2], color: `text` })}>
+      {post.title}
+    </Link>
+    <p sx={{ color: `secondary`, mt: 1, a: { color: `secondary` }, fontSize: [1, 1, 1] }}>
+      <time>{post.date}</time>
+      {post.tags && showTags && (
+        <React.Fragment>
+          {` — `}
+          <ItemTags tags={post.tags} />
+        </React.Fragment>
+      )}
+    </p>
+  </Box>
+)
+
+export default BlogListItem
